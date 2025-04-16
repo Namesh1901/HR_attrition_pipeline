12174fc6ad213b22527414d3b4cdeb3d03f85ea9
Namesh Dwivedi <nameshdwivedi5@gmail.com>
2025-04-16 18:51:36 +0530

    Add files via upload

--- dbt/models/intermediate/int_hr_attrition_features.sql
+++ dbt/models/intermediate/int_hr_attrition_features.sql
@@ -1,1 +1,14 @@
+-- models/fct_employee_attrition.sql
+WITH base AS (
+    SELECT * FROM {{ ref('stg_hr_employee_attrition') }}
+)
+
+SELECT
+    *,
+    CASE
+        WHEN yearsatcompany < 2 THEN '0-2 Years'
+        WHEN yearsatcompany BETWEEN 2 AND 5 THEN '2-5 Years'
+        ELSE '5+ Years'
+    END AS tenure_bucket
+FROM base
 
