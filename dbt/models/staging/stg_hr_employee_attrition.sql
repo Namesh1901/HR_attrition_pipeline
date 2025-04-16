d98305f0ff09c9317fe66622abfba6bf60af66a0
Namesh Dwivedi <nameshdwivedi5@gmail.com>
2025-04-16 18:48:42 +0530

    Add files via upload

--- dbt/models/staging/stg_hr_employee_attrition.sql
+++ dbt/models/staging/stg_hr_employee_attrition.sql
@@ -1,1 +1,17 @@
-
+-- Rename columns and filter NULLs
+SELECT
+    employeenumber,
+    age,
+    attrition,
+	JobRole,
+    department,
+    educationfield,
+    gender,
+    overtime,
+    jobsatisfaction,
+    worklifebalance,
+    monthlyincome,
+    totalworkingyears,
+    yearsatcompany
+FROM hr_employee_attrition
+WHERE employeenumber IS NOT NULL
