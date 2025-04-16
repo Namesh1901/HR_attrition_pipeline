7839744c55b6df0263c67605b9ccf72a09fca7ef
Namesh Dwivedi <nameshdwivedi5@gmail.com>
2025-04-16 18:52:09 +0530

    Add files via upload

--- dbt/models/final/fct_attrition_summary.sql
+++ dbt/models/final/fct_attrition_summary.sql
@@ -1,1 +1,9 @@
-
+-- Final table for dashboard
+SELECT
+    attrition,
+    gender,
+    department,
+    COUNT(*) AS employee_count,
+    AVG(monthlyincome) AS avg_income
+FROM {{ ref('int_hr_attrition_features') }}
+GROUP BY attrition, gender, department
